1e6a18e42aea69c659eef1f11cd384b57dff457f
lyzer <laicaiwang@dingding.com>
2020-04-20 13:43:07 +0800

    update HTML

---
+++ ZhiDaiHuiPC/files/登录/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="登录.html",d="generationDate",e=new Date(1573177991374.2),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="111b43c323ee4d6cace4b757517bdb52",q="type",r="Axure:Page",s="name",t="登录",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="79c01cd6a6d34375b887e190801fa3f5",Y="label",Z="friendlyType",ba="图片 ",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=1440,bj="height",bk=861.428571428571,bl="imageOverrides",bm="images",bn="normal~",bo="images/登录/u0.jpg",bp="masters",bq="objectPaths",br="79c01cd6a6d34375b887e190801fa3f5",bs="scriptId",bt="u0";
+return _creator();
+})());
